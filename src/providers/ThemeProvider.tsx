12fbafa395110255c520967f5880424f1b88bb1e
tundera <tunderadev@gmail.com>
2021-04-11 19:10:03 -0600

    init commit

---
+++ src/providers/ThemeProvider.tsx
@@ -0,0 +1,15 @@
+import type { FC } from 'react'
+
+import { ChakraProvider } from '@chakra-ui/react'
+import { Global } from '@emotion/react'
+import fonts from 'styles/font-face'
+import { theme } from 'styles/theme'
+
+export const ThemeProvider: FC = ({ children }) => {
+  return (
+    <ChakraProvider theme={theme}>
+      <Global styles={fonts} />
+      {children}
+    </ChakraProvider>
+  )
+}
---
+++ styles/theme/index.ts
@@ -0,0 +1,29 @@
+import { extendTheme, ThemeOverride } from '@chakra-ui/react'
+import colors from './colors'
+import fonts from './fonts'
+import styles from './styles'
+
+// Foundational style overrides
+// import borders from './foundations/borders'
+
+// Component style overrides
+// import Button from './components/button'
+// import Menu from './components/menu'
+// import Badge from './components/badge'
+
+const overrides: ThemeOverride = {
+  config: {
+    initialColorMode: 'light',
+    useSystemColorMode: false,
+  },
+  styles,
+  fonts,
+  colors,
+  components: {
+    // Button,
+    // Menu,
+    // Badge,
+  },
+}
+
+export const theme = extendTheme(overrides)
