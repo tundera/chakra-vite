9423e17f30b92019766bc1a543204604e488fe0f
tundera <tunderadev@gmail.com>
2021-04-12 10:03:54 -0600

    heading color

--- src/routes/Features.tsx
+++ src/routes/Features.tsx
@@ -1,12 +1,15 @@
+import { useColorModeValue } from '@chakra-ui/color-mode'
 import { Flex, Heading } from '@chakra-ui/layout'
 import FeatureCards from 'components/FeatureCards'
 import MainLayout from 'layouts/MainLayout'
 
 function Features() {
+  const headingColor = useColorModeValue('black', 'white')
+
   return (
     <MainLayout>
       <Flex direction="column" alignItems="center">
-        <Heading as="h1" fontSize="4xl" mt="8">
+        <Heading as="h1" fontSize="4xl" mt="8" color={headingColor}>
           Features
         </Heading>
         <FeatureCards />
